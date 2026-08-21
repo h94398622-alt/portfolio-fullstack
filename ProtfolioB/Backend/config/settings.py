479d63f013279e2961456b1fa6ee8f05164d9342
fathima hiba <fathimahibakarumbil@gmail.com>
2026-08-21 07:59:20 -0700

    Fix CORS for production frontend

--- ProtfolioB/Backend/config/settings.py
+++ ProtfolioB/Backend/config/settings.py
@@ -232,7 +232,10 @@ MEDIA_ROOT = BASE_DIR / "media"
 # =========================================================
 
 CORS_ALLOWED_ORIGINS = [
+    
     "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://portfolio-frontend-c0cg653mi-hiba-8da6.vercel.app",
 ]
 
 
