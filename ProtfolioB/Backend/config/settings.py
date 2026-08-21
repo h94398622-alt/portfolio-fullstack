94f15f987e105aa76b6005d2e8b261c75b6dc58f
fathima hiba <fathimahibakarumbil@gmail.com>
2026-08-21 09:28:32 -0700

    Update CORS origin

--- ProtfolioB/Backend/config/settings.py
+++ ProtfolioB/Backend/config/settings.py
@@ -235,7 +235,7 @@ CORS_ALLOWED_ORIGINS = [
     
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://portfolio-frontend-c0cg653mi-hiba-8da6.vercel.app",
+    "https://portfolio-frontend-weld-pi.vercel.app",
 ]
 
 
