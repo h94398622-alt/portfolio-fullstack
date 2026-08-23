5392cb143cdd52a64079a583c2fd4ccb63ebf3b7
fathima hiba <fathimahibakarumbil@gmail.com>
2026-08-23 11:06:49 -0700

    Move CORS middleware to top

--- ProtfolioB/Backend/config/settings.py
+++ ProtfolioB/Backend/config/settings.py
@@ -74,25 +74,39 @@ INSTALLED_APPS = [
 # MIDDLEWARE
 # =========================================================
 
-MIDDLEWARE = [
-    "django.middleware.security.SecurityMiddleware",
+# MIDDLEWARE = [
+#     "django.middleware.security.SecurityMiddleware",
 
-    # CORS middleware MUST be before CommonMiddleware
+#     # CORS middleware MUST be before CommonMiddleware
+#     "corsheaders.middleware.CorsMiddleware",
+
+#     "django.contrib.sessions.middleware.SessionMiddleware",
+#     "django.middleware.common.CommonMiddleware",
+#     "whitenoise.middleware.WhiteNoiseMiddleware",
+
+#     "django.middleware.csrf.CsrfViewMiddleware",
+
+#     "django.contrib.auth.middleware.AuthenticationMiddleware",
+
+#     "django.contrib.messages.middleware.MessageMiddleware",
+
+#     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+# ]
+
+
+MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
 
+    "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
 
     "django.middleware.csrf.CsrfViewMiddleware",
-
     "django.contrib.auth.middleware.AuthenticationMiddleware",
-
     "django.contrib.messages.middleware.MessageMiddleware",
-
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
-
 
 # =========================================================
 # URL CONFIGURATION
